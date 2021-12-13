479e8f408cfa68de69601ee69d4c534ec3b9f318
JenJohnston <Jen.Johnston@protonmail.ch>
2021-12-13 15:43:11 -0700

    rhubarb project site

---
+++ all-things-rhubarb/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+})
